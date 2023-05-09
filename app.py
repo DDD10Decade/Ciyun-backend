4588fb9e79b6b89fce3bfbc7259b9e898f6fbe41
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-05-09 21:54:30 +0800

    中文字体编码

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
 
 # 真正调用词云库生成图片
 def get_word_cloud(text):
-    # font = "./SimHei.ttf"
-    # pil_img = WordCloud(width=500, height=500, font_path=font).generate(text=text).to_image()
-    pil_img = WordCloud(width=800, height=300, background_color="white").generate(text=text).to_image()
+    font = r'C:\Windows\Fonts\simfang.ttf'  # 安装中文字体
+    pil_img = WordCloud(width=800, height=300, font_path=font, background_color="white").generate(text=text).to_image()
     img = io.BytesIO()
     pil_img.save(img, "PNG")
     img.seek(0)
